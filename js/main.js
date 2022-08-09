cd3dab888148c127cc1c9018f049b746638ed3af
puncoviy <usaspinner@gmail.com>
2022-08-09 22:17:31 +0300

    feat: finish responsive, add slider for providers

--- js/main.js
+++ js/main.js
@@ -2,6 +2,26 @@ let scroll = new SmoothScroll('a[href*="#"]', {
     speed: 300
 });
 
+$(function(){
+    $('.providers__list').slick({
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 6000,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
+
 const burgerBtn = document.querySelector('.burger-box');
 const burgerIcon = document.querySelector('.burger-menu');
 const menuList = document.querySelector('.header__menu-list');
